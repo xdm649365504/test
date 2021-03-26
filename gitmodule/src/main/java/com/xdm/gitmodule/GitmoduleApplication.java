6937de9fbd68f094cce2fb9ae2fd3ed8f928c7ad
xdm532526 <649365504@qq.com>
2021-03-26 20:39:12 +0800

    初始化

---
+++ gitmodule/src/main/java/com/xdm/gitmodule/GitmoduleApplication.java
@@ -0,0 +1,13 @@
+package com.xdm.gitmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitmoduleApplication.class, args);
+    }
+
+}
---
+++ gitmodule/src/test/java/com/xdm/gitmodule/GitmoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdm.gitmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitmoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
