e275e9bf064c08fd245fc7816f84af457b7e0c4d
xdm532526 <649365504@qq.com>
2021-03-26 21:51:35 +0800

    修改

--- gitmodule/src/main/java/com/xdm/gitmodule/GitmoduleApplication.java
+++ gitmodule/src/main/java/com/xdm/gitmodule/GitmoduleApplication.java
@@ -8,6 +8,7 @@ public class GitmoduleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitmoduleApplication.class, args);
+        System.out.println(123);
     }
 
 }
